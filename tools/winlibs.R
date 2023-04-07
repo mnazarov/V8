24c9750841cebcfaae74383b5aa4b21dafc1b1d1
Jeroen Ooms <jeroenooms@gmail.com>
2023-04-07 17:58:30 +0200

    Test oldrel

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,12 @@
 # Build against UCRT build of libv8
 if(!file.exists('../windows/ucrt64/include/v8.h')){
-  cdn <- ifelse(isTRUE(grepl('ucrt', R.version$crt)),
-                'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-ucrt.pkg.tar.xz',
-                'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-msvcrt.tar.xz')
+  cdn <- if(getRcppVersion() < 4){
+    'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-6.2.414.50-win.tar.xz'
+  } else {
+    ifelse(isTRUE(grepl('ucrt', R.version$crt)),
+           'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-ucrt.pkg.tar.xz',
+           'https://github.com/jeroen/V8/releases/download/v3.6.0/v8-9.1.269.38-win-msvcrt.tar.xz')
+  }
   download.file(cdn, "libv8.tar.xz", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   untar('libv8.tar.xz', exdir = "../windows", tar = 'internal')
